ea9e65df4701ba37605280ba2b0b0bfa0b3b9b76
ZSully09 <zsullivan93@gmail.com>
2019-12-17 21:39:40 +0000

    initial user testing commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, confirmation: true, length: {within: 6..40}
+  validates :email, presence: true, :uniqueness => {:case_sensitive => true}
+  validates :password_confirmation, presence: true
 
 end
