12b854ad4d5abc82a332232ec94147def48c5158
ZSully09 <zsullivan93@gmail.com>
2019-12-19 01:25:21 +0000

    home page testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist"
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -10,9 +7,12 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-Capybara.javascript_driver = :poltergeist
 # Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist"
 
+Capybara.javascript_driver = :poltergeist
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
